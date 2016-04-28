8673e629f39106a9a0f31527f43383e2a346dbea
Jonathan Yaniv <johnitravolta@gmail.com>
2016-04-28 04:21:48 +0300

    Router for HTML snippets is implemented. Addition code might be needed for executing additional script tags inside the HTML snippets.

--- router.js
+++ router.js
@@ -1,18 +1,61 @@
+"use strict";
 (function ($) {
-  "use strict";
+  /**
+   * Updates the view with the html snippet downloaded using Ajax request.
+   * Caches the view contents for later use.
+   *
+   * @param hash the view's hash
+   */
+  function updateView(hash) {
+    if (cache[hash]) {
+      viewElement.innerHTML = cache[hash];
+      return;
+    }
+    var fileName = viewsRoot.concat(hash.substr(1), ".html");
+
+    $.ajax(fileName)
+        .done(function (data) {
+          viewElement.innerHTML = data;
+          cache[hash] = data;
+        });
+
+    console.log(fileName);
+  }
 
   if (!$) {
     console.error("jQuery is not loaded");
     return;
   }
 
+  var viewsRoot = "views/";
+  var cache = {};
+  var viewElement = $("#view")[0];
+  var defaultHash = $.find(".nav .active a")[0].getAttribute("href");
+
+  // event handler for nav bar view change
+  $(".nav a").on("click", function () {
+    $("nav .active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
+
   // set event handler for hash change
   $(window).on('hashchange', function () {
-    //TODO: implement
-    console.log(window.location.hash);
+    if (!window.location.hash) {
+      window.location.hash = defaultHash;
+      return;
+    }
+
+    updateView(window.location.hash);
   });
 
   $(window).on('load', function () {
-    this.location.hash = $(".nav .active a")[0].getAttribute("href");
+    // user requested specific view
+    if (this.location.hash) {
+      var el = $.find(".nav a[href='".concat(this.location.hash, "']"))[0];
+      el.click();
+      $(this).trigger('hashchange');
+    } else {
+      this.location.hash = defaultHash;
+    }
   });
 })(window.jQuery);
